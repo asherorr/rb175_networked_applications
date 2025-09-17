535df11550e3c227ad36f625e22d058b30d3138f
asherorr <asher@mailfence.com>
2025-09-17 14:04:30 -0400

    This commit will enable users to add a todo item to an existing list. It will also validate the user's entry for the todo item.

--- personal_task_manager_with_heroku/todo.rb
+++ personal_task_manager_with_heroku/todo.rb
@@ -37,6 +37,14 @@ def error_for_list_name(name)
   end
 end
 
+# Return an error message if the todo is invalid
+# Returns nil if name is valid
+def error_for_todo(name)
+  if !(1..100).cover? name.size
+    "Todo must be between 1 and 100 characters."
+  end
+end
+
 # Create a new list
 post "/lists" do
   list_name = params[:list_name].strip
@@ -91,4 +99,21 @@ post "/lists/:id/destroy" do
   session[:lists].delete_at(id)
   session[:success] = "The list was deleted."
   redirect "/lists"
+end
+
+# Add an new todo item to a list
+post "/lists/:list_id/todos" do
+  @id = params[:list_id].to_i
+  @list = session[:lists][@id]
+  text = params[:todo].strip
+
+  error = error_for_todo(text)
+  if error
+    session[:error] = error
+    erb :list, layout: :layout
+  else
+    @list[:todos] << {name: text, completed: false}
+    session[:success] = "The todo was added."
+    redirect "/lists/#{@id}"
+  end
 end
