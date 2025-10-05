a4509ddc48ad491e6451ac9960c5d27fe9af1997
asherorr <asher@mailfence.com>
2025-10-05 15:16:55 -0400

    This commit will present users with a list of links upon visiting the / page. There will be a link for each document in the cms. When they click a link, they will be taken to a page that displays the contents of that file.

--- file_based_cms/cms.rb
+++ file_based_cms/cms.rb
@@ -8,3 +8,13 @@ get "/" do
   erb :index
 end
 
+#make a route for each file in the data folder
+#build a route with params to capture the file
+#show the contents of the page
+
+get "/data/:file" do
+  file_name = params[:file]
+
+  @contents = File.read("data/#{file_name}")
+  erb :file
+end
