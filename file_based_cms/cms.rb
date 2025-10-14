0fbb7a68f397121a5eca25dbefc8959d8ecd80df
asherorr <asher@mailfence.com>
2025-10-14 16:11:37 -0400

    This commit will enable sessions in the application and redirect users to the index page with a one-time error message upon attempting to a view a document that doesn't exist.

--- file_based_cms/cms.rb
+++ file_based_cms/cms.rb
@@ -2,19 +2,35 @@ require "sinatra"
 require "sinatra/reloader"
 require "sinatra/content_for"
 require "tilt/erubi"
+require "securerandom"
+
+configure do
+  enable :sessions
+  set :session_secret, SecureRandom.hex(32)
+end
+
+helpers do
+  def display_flash
+    return unless session[:error]
+    %(<div class="flash error"><h1>#{session.delete(:error)}</h1></div>)
+  end
+end
+
+
+def get_all_files
+  Dir.glob("data/*").map { |path| File.basename(path) }
+end
 
 get "/" do
-  @files = Dir.glob("data/*").map { |path| File.basename(path) }
+  @files = get_all_files
   erb :index
 end
 
-#make a route for each file in the data folder
-#build a route with params to capture the file
-#show the contents of the page
-
 get "/data/:file" do
   file_name = params[:file]
-
   @contents = File.read("data/#{file_name}")
   erb :file
+  rescue Errno::ENOENT
+    session[:error] = "The file '#{file_name}' doesn't exist."
+    redirect "/"
 end
