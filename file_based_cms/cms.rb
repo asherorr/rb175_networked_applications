89af1d4b943784ad266335b82975a360232636c5
asherorr <asher@mailfence.com>
2025-10-15 12:46:48 -0400

    Centralize data path configuration and add test for nonexistent file handling
    - Define settings.data_path in cms.rb to standardize file lookups
    - Configure a separate test data directory for RACK_ENV=test
    - Update cms_test.rb to seed and clean test data before/after tests
    - Add test to confirm redirect when a user views a nonexistent file

--- file_based_cms/cms.rb
+++ file_based_cms/cms.rb
@@ -7,6 +7,12 @@ require "securerandom"
 configure do
   enable :sessions
   set :session_secret, SecureRandom.hex(32)
+  set :data_path, File.expand_path("../data", __dir__)
+end
+
+configure :test do
+  # When RACK_ENV=test, use test fixtures
+  set :data_path, File.expand_path("../test/data", __dir__)
 end
 
 helpers do
@@ -16,9 +22,8 @@ helpers do
   end
 end
 
-
 def get_all_files
-  Dir.glob("data/*").map { |path| File.basename(path) }
+  Dir.glob(File.join(settings.data_path, "*")).map { |path| File.basename(path) }
 end
 
 get "/" do
@@ -28,7 +33,9 @@ end
 
 get "/data/:file" do
   file_name = params[:file]
-  @contents = File.read("data/#{file_name}")
+  file_path = File.join(settings.data_path, file_name)
+
+  @contents = File.read(file_path)
   erb :file
   rescue Errno::ENOENT
     session[:error] = "The file '#{file_name}' doesn't exist."
--- file_based_cms/test/cms_test.rb
+++ file_based_cms/test/cms_test.rb
@@ -2,6 +2,7 @@ ENV["RACK_ENV"] = "test"
 
 require "minitest/autorun"
 require "rack/test"
+require "fileutils"
 
 require_relative "../cms"
 
@@ -10,6 +11,18 @@ class AppTest < Minitest::Test
 
   def app
     Sinatra::Application
+  end
+
+  def setup
+    @data_path = app.settings.data_path
+    FileUtils.mkdir_p(@data_path)
+    File.write(File.join(@data_path, "about.txt"),   "Ruby is...")
+    File.write(File.join(@data_path, "changes.txt"), "Recent Ruby changes")
+    File.write(File.join(@data_path, "history.txt"), "History of Ruby")
+  end
+
+  def teardown
+    FileUtils.rm_f Dir[File.join(@data_path, "*")]
   end
 
   def test_index
@@ -25,5 +38,11 @@ class AppTest < Minitest::Test
     assert_equal 200, last_response.status
     assert_equal "text/html;charset=utf-8", last_response["Content-Type"]
     assert_includes last_response.body, "Ruby"
+  end
+
+  def test_index_if_user_views_nonexistent_file
+    nonexistent_file = "data/#{Time.now.to_i}_#{SecureRandom.hex(2)}.txt"
+    get "data/#{nonexistent_file}"
+    assert_equal 302, last_response.status
   end
 end
